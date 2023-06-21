5ae9c681c4949ae4974f82d318cde79649220d17
Matheus <rdesouzamatheus@gmail.com>
2023-06-20 22:02:39 -0300

    Criada a classe Message com suas propriedades que se tornarão uma tabela no banco de dados

---
+++ Entities/Entities/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    internal class Message
+    {
+        [Column("MSN_ID")]
+        public int id { get; set; }
+        [Column("MSN_TITLE")]
+        [StringLength(255)]
+        public string title { get; set; }
+        [Column("MSN_ISACTIVATED")]
+        public bool isActivated { get; set; }
+        [Column("MSN_REGISTERTIME")]
+        public DateTime registerDate { get; set; }
+        [Column("MSN_ALTTIME")]
+        public DateTime altTime { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public int userId { get; set; }
+        public ApplicationUser applicationUser { get; set; }
+    }
+}
