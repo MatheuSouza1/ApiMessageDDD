28130aeb24e371fa39c2b1f0be555881be102506
Matheus <rdesouzamatheus@gmail.com>
2023-06-23 19:22:35 -0300

    Criado Repositorio generico, seus métodos CRUD basicos e criado a classe RepositoryMessage

---
+++ Infraestructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,100 @@
+﻿using Domain.Interfaces.Generics;
+using Infraestructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+
+        public RepositoryGenerics()
+        {
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+        public async Task Add(T entity)
+        {
+            using(var data = new ContextBase(_optionsBuilder))
+            {
+                await data.Set<T>().AddAsync(entity);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T entity)
+        {
+            using(var data = new ContextBase(_optionsBuilder))
+            {
+                data.Set<T>().Remove(entity);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int id)
+        {
+            using(var data = new ContextBase(_optionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using(var data = new ContextBase(_optionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task Update(T entity)
+        {
+            using(var data = new ContextBase(_optionsBuilder))
+            {
+                data.Set<T>().Update(entity);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        //implementação da interface IDisposable abaixo:
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+        // fim
+    }
+}
---
+++ Infraestructure/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infraestructure.Configuration;
+using Infraestructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository.Repositories
+{
+    public class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+        public RepositoryMessage() 
+        {
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+    }
+}
